714b21bd17222f7978bbc8f21bca41b872a655f0
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-03-02 12:15:51 +0330

    Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CustomPipePipe } from './custom-pipe.pipe';
+import { CutStrPipe } from './cut-str.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CustomPipePipe, CutStrPipe],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/cut-str.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CutStrPipe } from './cut-str.pipe';
+
+describe('CutStrPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CutStrPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
