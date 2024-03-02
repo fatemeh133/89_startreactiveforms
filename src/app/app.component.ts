4c333a393b9839a58ad33df2a25193d965e83705
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-03-02 10:00:20 +0330

    add family reset pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'formhandling';
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  sayHi = 'Hi welcome';
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null),
+        email: new FormControl('null@gmail.com'),
+      }),
+      gender: new FormControl('male'),
+      family: new FormArray([]),
+    });
+    this.signupForm.patchValue({
+      userData: { username: 'example username' },
+    });
+  }
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+  addFamily() {
+    const controls = new FormControl(null);
+    (<FormArray>this.signupForm.get('family'))?.push(controls);
+  }
+
+  get castedarraycontrols() {
+    return (<FormArray>this.signupForm.get('family')).controls;
+  }
+  reset() {
+    this.signupForm.reset();
+  }
 }
